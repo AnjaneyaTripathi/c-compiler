bbd9dad6633bb29b9cf7aed5117b67545f78e35f
Anjaneya Tripathi <anjaneya532000@gmail.com>
2021-10-28 10:23:40 +0530

    modified file names

--- input2.c
+++ input2.c
@@ -2,23 +2,10 @@
 #include<string.h>
 
 int main() {
-    int x=1;
-    float f;
-    int a=3;
-    int x;
-    a = x * 3 + 5;
-    if(x>a) {
-        printf("Hi!");
-        a = x * 3 + 100;
-        if(x>a) {
-            printf("Hi!");
-            a = x * 3 + 100;
-        }
-        else {
-            x = a * 3 + 100;
-        }
-    }
-    else {
-        x = a * 3 + 100;
-    }
+    int i=1;
+    float f = 2.5;
+    char c = 'A';
+    int x = 3.5;
+    i = x + f * c;
+    return 3;
 }
--- input3.c
+++ input3.c
@@ -1,11 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 
 int main() {
-    int x;
-    char c;
+    int x=1;
     float f;
-    x = 1.05;
-    c = 'A';
-    f = 3;
-    x = c * f;
+    int a=3;
+    int x;
+    a = x * 3 + 5;
+    if(x>a) {
+        printf("Hi!");
+        a = x * 3 + 100;
+        if(x>a) {
+            printf("Hi!");
+            a = x * 3 + 100;
+        }
+        else {
+            x = a * 3 + 100;
+        }
+    }
+    else {
+        x = a * 3 + 100;
+    }
 }
