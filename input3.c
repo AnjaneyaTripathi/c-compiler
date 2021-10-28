32a7f432dc848821acc231ad87abd0395c5a3703
Anjaneya Tripathi <anjaneya532000@gmail.com>
2021-10-28 10:19:07 +0530

    modified input files

--- input3.c
+++ input3.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-#include<string.h>
 
 int main() {
-    int i=1;
-    float f = 2.5;
-    char c = 'A';
-    int x = 3.5;
-    i = f*c + x;
-    return 3.5;
+    int x;
+    char c;
+    float f;
+    x = 1.05;
+    c = 'A';
+    f = 3;
+    x = c * f;
 }
